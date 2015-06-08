f630a2574bd6b692a48e6932c6e7f264c172c7c0
jbarney86 <jefferydale1986@gmail.com>
2015-06-08 00:21:26 -0600

    Update extension.js

--- extension.js
+++ extension.js
@@ -119,6 +119,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/yemasthui/basicBot/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/masterjh/basicBot/master/basicBot.js", extend);
 
 }).call(this);
